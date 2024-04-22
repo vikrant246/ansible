3577827791c35df868fa33300cb7ec891d1e48ad
unknown <you@example.com>
2024-04-22 08:15:25 +0530

    ansible_commit

---
+++ src/main/java/ansible/assigment/ansible/AnsibleApplication.java
@@ -0,0 +1,13 @@
+package ansible.assigment.ansible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ansible/assigment/ansible/AnsibleApplicationTests.java
@@ -0,0 +1,13 @@
+package ansible.assigment.ansible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
